4356bf3756bb201fd449e5a62b5572a36ae80499
Xpow22 <diskaesfandiary22@gmail.com>
2022-05-25 19:26:16 +0700

    upload code

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { FaBeer } from "react-icons/fa";
+
+function App() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <FaBeer />
+      <form>
+        
+          <input type="text" placeholder="Usernamennyyyaaa" />
+
+      </form>
+      <form>
+        <input type ="password" placeholder="password" /> 
+      </form>
+    </div>
+  );
+}
+
+export default App;

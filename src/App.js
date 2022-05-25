46c9f53c937708f35ab43ab990c58f25164c99e1
Xpow22 <diskaesfandiary22@gmail.com>
2022-05-25 19:39:22 +0700

    add button

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import { FaBeer } from "react-icons/fa";
 function App() {
   return (
     <div>
-      <h1>Login</h1>
+      <h1 className="title">Login</h1>
       <FaBeer />
       <form>
-        
           <input type="text" placeholder="Usernamennyyyaaa" />
-
       </form>
       <form>
         <input type ="password" placeholder="password" /> 
       </form>
       <button>Klik</button>
+      <button>Klik2</button>
     </div>
   );
 }

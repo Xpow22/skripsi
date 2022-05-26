2d10fa7f9535c7d59bb3c96f0c21e8e6d21df00b
alilatukau03 <alilatukau07@gmail.com>
2022-05-26 13:29:09 +0700

    done struktur

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import "./App.css";
-import { FaBeer } from "react-icons/fa";
-
 function App() {
   return (
     <div>
       <h1 className="title">Login</h1>
-      <FaBeer />
       <form>
-          <input type="text" placeholder="Usernamennyyyaaa" />
+        <input type="text" placeholder="Usernamennyyyaaa" />
       </form>
       <form>
-        <input type ="password" placeholder="password" /> 
+        <input type="password" placeholder="password" />
       </form>
       <button>Klik</button>
       <button>Klik2</button>
